dcf6592dc1d3a8dc5a92e8f4a5d27341faeb7d38
ijlal007 <mi04892@st.habib.edu.pk>
2023-01-20 12:24:07 +0500

    6th

--- lib/widgets/song_card.dart
+++ lib/widgets/song_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 import '../models/song_model.dart';
 
@@ -13,13 +14,53 @@ class SongCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: MediaQuery.of(context).size.width * 0.4,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10),
-        image: DecorationImage(
-          image: AssetImage(song.coverUrl),
-          fit: BoxFit.cover,
-        ),
+      margin: const EdgeInsets.only(right: 10),
+      child: Stack(
+        alignment: Alignment.bottomCenter,
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width * 0.4,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              image: DecorationImage(
+                image: AssetImage(song.coverUrl),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Container(
+            height: 50.0,
+            width: MediaQuery.of(context).size.width * 0.35,
+            margin: const EdgeInsets.only(bottom: 10),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+                  Colors.grey.withOpacity(0.8),
+                  Colors.white.withOpacity(0.7),
+                ],
+              ),
+            ),
+            child: Column(children: [
+              const SizedBox(height: 5),
+              Text(
+                song.title,
+                style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                    ),
+              ),
+              Text(
+                song.description,
+                style: Theme.of(context).textTheme.bodySmall?.copyWith(
+                      color: Colors.black,
+                    ),
+              ),
+            ]),
+          )
+        ],
       ),
     );
   }
